5b28af3b7d3d1977aaebfc76c715274dccb8b91d
Himanshi Gupta <gupta.himanshi3288@gmail.com>
2021-04-17 18:28:17 +0100

    added required services.

---
+++ SynetecAssessmentApi/Services/IBonusCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SynetecAssessmentApi.Services
+{
+    /// <summary>
+    /// Service responsible for calculation of Bonus.
+    /// </summary>
+    public interface IBonusCalculator
+    {
+    
+        /// <summary>
+        /// Method responsible for calculating bonus for an employee.
+        /// </summary>
+        /// <param name="employeeId">Id of the Employee</param>
+        /// <returns>
+        /// Bonus Amount.
+        /// Throws EmployeeNotFound exception in case employee is not found in the system.
+        /// </returns>
+        Decimal Calculate(int employeeId);
+    }
+}
---
+++ SynetecAssessmentApi/Services/IEmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using SynetecAssessmentApi.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SynetecAssessmentApi.Services
+{
+
+    /// <summary>
+    /// Service responsible for providing contracts for Employees.
+    /// </summary>
+    public interface IEmployeeService
+    {
+        /// <summary>
+        /// Get all available employees.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<EmployeeDto>> GetAsync();
+
+        /// <summary>
+        /// Gets employee on the basis of Id.
+        /// </summary>
+        /// <param name="id">Id for the employee</param>
+        /// <returns>Employee object Or NULL.</returns>
+        Task<EmployeeDto> Get(int id);
+
+        /// <summary>
+        /// Get Queryable reference to Employee list. 
+        /// This enables to perform operations like SUM, COUNT directly on dbContext. But also does not exposes dbContext out of service.
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<EmployeeDto> GetQueryable();
+    }
+}
